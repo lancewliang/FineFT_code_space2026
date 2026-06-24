0e5021b94e19aab7c3ce0021d4bf1d19e968732b
lance.liang <78599194@qq.com>
2026-06-24 22:40:17 +0800

    1

---
+++ FineFT/script/train/train_commodity.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOTPATH=${ROOTPATH:-$(pwd)}
+/home/lanceliang/miniconda3/bin/conda run -n finetf env PYTHONPATH="${ROOTPATH}/FineFT" nohup python -u FineFT/RL/DiHFT/low_level/weight_advantage_pretrain.py --dataset_name fu --max_holding_number 10 >log_futures/fu/low_level/train/advantage.log 2>&1 &
