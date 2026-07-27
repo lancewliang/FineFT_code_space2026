84cabfac8cba2bb917347de67f835513aada8369
lance.liang <78599194@qq.com>
2026-07-27 21:31:04 +0800

    bug

--- FineFT/RL/DiHFT/low_level/weight_advantage_pretrain.py
+++ FineFT/RL/DiHFT/low_level/weight_advantage_pretrain.py
@@ -643,6 +643,7 @@ class Weighted_Contexts_DQN:
         time_input = torch.cat([hour_count_down, minute_count_down], dim=1).to(
             self.device
         )
+        trading_info = info["trading_info"].float().to(self.device)
         # next input
         states_ = next_states.reshape(bs, -1)
         previous_action_ = info_["previous_action"].float().unsqueeze(1)
@@ -652,6 +653,7 @@ class Weighted_Contexts_DQN:
         time_input_ = torch.cat([hour_count_down_, minute_count_down_], dim=1).to(
             self.device
         )
+        trading_info_ = info_["trading_info"].float().to(self.device)
 
         current_sa_quantiles = evaluate_quantile_at_action(
             self.eval_net(
@@ -670,6 +672,7 @@ class Weighted_Contexts_DQN:
                 time=time_input_,
                 previous_action=previous_action_,
                 avaliable_action=avaliable_action_,
+                trading_info=trading_info_,
             )
             next_sa_quantiles = next_q.unsqueeze(1)
             assert next_sa_quantiles.shape == (self.batch_size, 1, self.N)
@@ -693,6 +696,7 @@ class Weighted_Contexts_DQN:
             time=time_input,
             previous_action=previous_action,
             avaliable_action=avaliable_action,
+            trading_info=trading_info,
         )
         assert predict_action_distrbution.shape == (
             self.batch_size,
@@ -741,6 +745,7 @@ class Weighted_Contexts_DQN:
         time_input = torch.cat([hour_count_down, minute_count_down], dim=1).to(
             self.device
         )
+        trading_info = info["trading_info"].float().to(self.device)
         # next input
         states_ = next_states.reshape(bs, -1)
         previous_action_ = info_["previous_action"].float().unsqueeze(1)
@@ -750,6 +755,7 @@ class Weighted_Contexts_DQN:
         time_input_ = torch.cat([hour_count_down_, minute_count_down_], dim=1).to(
             self.device
         )
+        trading_info_ = info_["trading_info"].float().to(self.device)
 
         current_sa_quantiles = evaluate_quantile_at_action(
             self.eval_net(
@@ -769,6 +775,7 @@ class Weighted_Contexts_DQN:
                 time=time_input_,
                 previous_action=previous_action_,
                 avaliable_action=avaliable_action_,
+                trading_info=trading_info_,
             )
             next_sa_quantiles = next_q.unsqueeze(1)
             assert next_sa_quantiles.shape == (self.batch_size, 1, self.N)
@@ -793,6 +800,7 @@ class Weighted_Contexts_DQN:
             time=time_input,
             previous_action=previous_action,
             avaliable_action=avaliable_action,
+            trading_info=trading_info,
         )
         assert predict_action_distrbution.shape == (
             self.batch_size,
